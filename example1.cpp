9c2cf4423848d1f3e813972437216861b70a2057
Stephen I. Killgore <45843593+stephen45ss@users.noreply.github.com>
2019-09-17 04:21:36 -0500

    Add files via upload

---
+++ example1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> /* input outputstream */
+#include <string.h> /* need this so you can use string declaration */
+#include <windows.h> /* the windows header file you need this for windows programs */
+using namespace std; /* you need this so you dont have to std:: infront of cout ever time you wan to use it */
+
+int main() {
+string monster = "Bear";
+cout << monster << "\n";
+system("pause");
+return(0);
+}
